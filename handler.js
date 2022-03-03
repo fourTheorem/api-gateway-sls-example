0ad0646ec1f75df337745bfc817c045e3c0f3611
Luciano Mammino <luciano.mammino@fourtheorem.com>
2022-03-03 21:39:32 +0000

    Added example on how to write to s3

--- handler.js
+++ handler.js
@@ -1,5 +1,9 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3')
+
+const s3 = new S3Client()
+const { BUCKET_NAME } = process.env
 
 module.exports.getNews = async function getNews (event) {
   // logs the event (good practice for debugging)
@@ -19,6 +23,13 @@ module.exports.getNews = async function getNews (event) {
       link: $(entry).attr('href').trim()
     }
   })
+
+  // save the data to S3
+  await s3.send(new PutObjectCommand({
+    Key: `${Date.now()}.json`,
+    Body: JSON.stringify(news, null, 2),
+    Bucket: BUCKET_NAME
+  }))
 
   // returns an HTTP response using the format expected
   // by API Gateway (Lambda proxy integraiton)
